299f5a031cdc8f7aa559c5e6b07d160831519e3a
“AbdurRaahimm” <rahim703936@gmail.com>
2024-10-23 23:38:29 +0600

    shadcn install

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,44 +1,52 @@
-export const loadFFmpeg = async (url: string): Promise<HTMLScriptElement> => {
-    return new Promise((resolve, reject) => {
-      const script: HTMLScriptElement = document.createElement("script");
-      let loaded: boolean = false;
-      script.async = true;
-      script.defer = true;
-      script.src = url;
-  
-      script.onload = () => {
-        if (!loaded) {
-          resolve(script);
-        }
-        loaded = true;
-      };
-  
-      script.onerror = () => {
-        console.error("Failed to load ffmpeg.wasm");
-        reject(new Error("Failed to load ffmpeg.wasm"));
-      };
-  
-      document.getElementsByTagName("head")[0].appendChild(script);
-    });
-  };
-  
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
 
-  export const convertToHHMMSS = (seconds: number) => {
-    const date = new Date(seconds * 1000);
-    const hh = date.getUTCHours();
-    const mm = date.getUTCMinutes();
-    const ss = date.getUTCSeconds();
-    if (hh) {
-        return `${hh}:${mm.toString().padStart(2, "0")}:${ss
-            .toString()
-            .padStart(2, "0")}`;
-    } else {
-        return `${mm.toString().padStart(2, "0")}:${ss
-            .toString()
-            .padStart(2, "0")}`;
-    }
-  
-    return `${hh}:${mm.toString().padStart(2, "0")}:${ss
-        .toString()
-        .padStart(2, "0")}`;
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+
+export const loadFFmpeg = async (url: string): Promise<HTMLScriptElement> => {
+  return new Promise((resolve, reject) => {
+    const script: HTMLScriptElement = document.createElement("script");
+    let loaded: boolean = false;
+    script.async = true;
+    script.defer = true;
+    script.src = url;
+
+    script.onload = () => {
+      if (!loaded) {
+        resolve(script);
+      }
+      loaded = true;
+    };
+
+    script.onerror = () => {
+      console.error("Failed to load ffmpeg.wasm");
+      reject(new Error("Failed to load ffmpeg.wasm"));
+    };
+
+    document.getElementsByTagName("head")[0].appendChild(script);
+  });
+};
+
+
+export const convertToHHMMSS = (seconds: number) => {
+  const date = new Date(seconds * 1000);
+  const hh = date.getUTCHours();
+  const mm = date.getUTCMinutes();
+  const ss = date.getUTCSeconds();
+  if (hh) {
+      return `${hh}:${mm.toString().padStart(2, "0")}:${ss
+          .toString()
+          .padStart(2, "0")}`;
+  } else {
+      return `${mm.toString().padStart(2, "0")}:${ss
+          .toString()
+          .padStart(2, "0")}`;
+  }
+
+  return `${hh}:${mm.toString().padStart(2, "0")}:${ss
+      .toString()
+      .padStart(2, "0")}`;
 }
